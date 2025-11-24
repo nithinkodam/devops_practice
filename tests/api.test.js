e0d9b9243d7283089ba0c02c99d7c8b2f619f8e6
nithinkodam <nithinkodam69@gmail.com>
2025-11-24 21:56:12 +0530

    Fix tests for CI

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../server"); // update path if needed
+const app = require("../index"); // update path if needed
 
 describe("API Tests", () => {
   it("GET / should return status ok", async () => {
